a43a2435af62a15c1313213b9a4e2552e34ee739
Raymundo Mateos <rayma9829@gmail.com>
2023-02-17 01:57:51 -0600

    [ADD] Funcionalidad para cambiar de imagen, pasando todas las imagenes se regresa a la primera sin importar cuantas imagenes se agreguen en el html

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+let currentActive = 0;
+let imagesNum = document.querySelectorAll('.image').length;
 let lastButton = document.getElementById('lastButton');
 let nextButton = document.getElementById('nextButton');
 
 function handleClick(e){
     this.classList.add('active');
+    let lastImage = document.getElementById(`image${currentActive%imagesNum}`);
+    lastImage.classList.remove('active');
+    if(this.id === 'lastButton') currentActive = (imagesNum + (currentActive - 1))%imagesNum;
+    else currentActive = (imagesNum + currentActive + 1)%imagesNum;
+    document.getElementById(`image${currentActive%imagesNum}`).classList.add('active');
 }
+
+
 
 function removeActive(e){
     if(e.propertyName !== 'background-color') return;
